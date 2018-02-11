5deb8157becde9be6bdc9fc08cfaae91a326bcd0
Kannagi <kannagichan@gmail.com>
2018-02-11 09:58:04 +0100

    remove bin

--- LMP3D/bin/DC/binDC.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/opt/toolchains/dc/sh-elf/bin/sh-elf-objcopy -O binary DC_demo.elf myfile.bin
-
-
-
-
--- LMP3D/bin/PSX/compilationPS1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm PS1_demo.iso
-export PATH=$PATH:/usr/local/psxsdk/bin
-elf2exe PS1_demo.elf demo.exe
-mkisofs -iso-level 1 -o PS1_demo.iso *
-
-
-pcsxr -cdfile PS1_demo.iso
-
