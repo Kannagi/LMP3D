3465dc694880f3950df4406cbe885dfad58728b2
Kannagi <kannagichan@gmail.com>
2018-04-26 18:53:56 +0200

    Delete binDC.sh

--- LMP3D/bin/DC/binDC.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp ../DC_demo.elf DC_demo.elf
-/opt/toolchains/dc/sh-elf/bin/sh-elf-objcopy -O binary DC_demo.elf DC_demo.bin
-wine /home/kannagi/Documents/SDK/dreamcast/bootmake_11_cdi4dc_02b/Tools/extra/scramble.exe DC_demo.bin 1ST_READ.BIN
-mv DC_demo.bin DS_CORE.BIN
-
-
-
-
