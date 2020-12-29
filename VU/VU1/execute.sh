155bb15bad84db9400bac93aabda967070a94a09
Kannagi <kannagi@localhost.localdomain>
2020-12-29 18:46:56 +0100

    New

---
+++ VU/VU1/execute.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cp PS2_vu1Triangle.c /home/kannagi/Documents/Projet/LMP3D/LMP3D/PS2/Graphics/PS2_vu1Triangle.c
--- VU/bashtest.sh
+++ VU/bashtest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/home/kannagi/Documents/SDK/PS2/ps2dev/dvp/bin/dvp-as vu1Triangle.vcm -o vu1Triangle.o
+/home/kannagi/Documents/SDK/PS2/ps2dev/dvp/bin/dvp-as Testvu1Triangle.vcm -o vu1Triangle.o
 /home/kannagi/Documents/SDK/PS2/ps2dev/ee/bin/ee-objcopy -O binary vu1Triangle.o vu1Triangle.bin
 /home/kannagi/Documents/SDK/PS2/ps2dev/ps2sdk/bin/bin2c vu1Triangle.bin PS2_vu1Triangle.c vu1Triangle
 
