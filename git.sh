277e8e422ea067a5e9ce68db8921bfd72b21ace3
Kannagi <kannagichan@gmail.com>
2021-01-23 23:14:36 +0100

    VU1

--- git.sh
+++ git.sh
@@ -5,7 +5,7 @@ git add VU/
 git add bin/
 git add LMP3D/
 git add *
-
+git rm LMP3D_lib.cbp
 git commit -m "$message"
 #git remote add origin https://github.com/Kannagi/LMP3D.git
 #git push -u origin master
