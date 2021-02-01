217c2eff924c28942720c74ed722a84cd608d7ef
Kannagi <kannagichan@gmail.com>
2021-02-01 16:28:09 +0100

    remove

--- git.sh
+++ git.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 read message
 echo \"$message\"
-git add VU/
-git add bin/
-git add LMP3D/
-git add *
-git rm LMP3D_lib.cbp
+
+git rm git.sh
 git commit -m "$message"
 #git remote add origin https://github.com/Kannagi/LMP3D.git
 #git push -u origin master
